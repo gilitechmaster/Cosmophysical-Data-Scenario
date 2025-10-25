dfee4127d9582d8cfedc2f796df5b2daaaf10ae5
경섭 <51491577+gilitechmaster@users.noreply.github.com>
2025-10-25 17:14:18 +0900

    Update 시나리오_3.cs

--- 시나리오_3.cs
+++ 시나리오_3.cs
@@ -2,6 +2,7 @@ using System;
 public class A
 {
   private int b = 1; // b는 private이므로 다른 클래스에서 사용할 수 없다.
+  //public int b = 1; // 위의 private 대신에 public 으로 쓰면 아래 this.b 에서 쓸 수 있다.
   public class B : A // B는 A에서 파생된다.
   {
     public int B1()
